c8460a7f2e3f745b73be02ed8683a8dd0039fe58
Saheer Babu <sheerbabu549@gmail.com>
2024-04-17 01:04:31 +0530

    style

--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@ uploadBtn.addEventListener("click", () => {
     upload()
 })
 function upload(){
+    uploadBtn.innerHTML=`<img src="./loading.gif" alt="">`
+    uploadBtn.classList.add("btn_loading")
 
     const path = document.querySelector("#path")
     let postRef = firebase.database().ref('shopless/' + path.value);
@@ -73,10 +75,27 @@ console.log(contents)
                 // setTimeout(clearColor, 300)
                 // setTimeout(clearUrl, 400)
 
-                alert("Success")
+                
+                uploadBtn.classList.remove("btn_loading")
+                uploadBtn.innerHTML=`<img class="doneGif" src="./done.gif" alt="">`
+                uploadBtn.style.border="solid 1px #41B06E"
+                uploadBtn.style.background="#222831"
+                setTimeout(success, 3000)
 
             })
             .catch(error => console.log(error));
+            document.querySelector(".error").innerHTML= error
+
+            uploadBtn.style.border="solid 1px red"
+            uploadBtn.innerHTML=`
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
+            `
+            setTimeout(success, 3000)
+
+            
 
     }
 
@@ -87,4 +106,10 @@ console.log(contents)
 
 //-------
 
-
+function success(){
+    uploadBtn.innerHTML=`Upload`
+    uploadBtn.style.background= "#76ABAE"
+    uploadBtn.style.color= "#EEEEEE"
+    uploadBtn.style.border= "none"
+    
+}
